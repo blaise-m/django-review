9efdf184171e708e06ec56a24662794640cd5581
blaise-m <mubangiziblaise@gmail.com>
2020-09-10 00:15:36 +0300

    Model, View, Controller setup with the Django admin

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from boards.models import Board
+
 
 def home(request):
-	return HttpResponse("Hello World!!!")
+	
+	boards = Board.objects.all()
+	
+	# boards_names = list()
+
+	# for board in boards:
+	# 	boards_names.append(board.name)
+
+	# response_html = '<br>'.join(boards_names)
+	
+	# return HttpResponse(response_html)
+
+	return render(request, 'home.html', {'boards': boards})
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
