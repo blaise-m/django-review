673ef2b1d97ab5a77179ef79c545a62379a6af35
blaise-m <mubangiziblaise@gmail.com>
2020-09-10 17:55:47 +0300

    Implement the new topic form the traditional way

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,31 +1,49 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 # from django.http import Http404   #HttpResponse
-from boards.models import Board
+from django.contrib.auth.models import User
+from boards.models import Board, Topic, Post
 
 
-def home(request):
+def boards(request):	
 	
-	boards = Board.objects.all()
-	
+	boards_list = Board.objects.all()	
+
 	# boards_names = list()
-
 	# for board in boards:
 	# 	boards_names.append(board.name)
-
-	# response_html = '<br>'.join(boards_names)
-	
+	# response_html = '<br>'.join(boards_names)	
 	# return HttpResponse(response_html)
 
-	return render(request, 'home.html', {'boards': boards})
+	return render(request, 'boards.html', {'boards': boards_list})
 
 
 def board_topics(request, pk):
-
+	
 	# try:
 	# 	board = Board.objects.get(pk=pk)
 	# except Board.DoesNotExist:
 	# 	raise Http404
 
 	board = get_object_or_404(Board, pk=pk)
+	topics = board.topics.all()
+	return render(request, 'topics.html', {'board': board, 'topics': topics})
 
-	return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):		
+	
+	board = get_object_or_404(Board, pk=pk)
+
+	if request.method == 'POST':
+		subject = request.POST['subject']
+		message = request.POST['message']
+
+		# TODO: Get the currently logged in user
+		user = User.objects.first()
+
+		topic = Topic.objects.create(subject=subject, board=board, starter=user)
+		post = Post.objects.create(message=message, topic=topic, created_by=user)
+
+		# TODO: Redirect to the created topic page
+		return redirect('board_topics', pk=board.pk)
+
+	return render(request, 'new_topic.html', {'board': board})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 
 from boards import views
 
 urlpatterns = [
-	path('', views.home, name='home'),
+	path('', lambda request: redirect('boards/', permanent=True)),
+	path('boards/', views.boards, name='boards'),
 	path('boards/<pk>/', views.board_topics, name='board_topics'),
+	path('boards/<pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls)    
 ]
